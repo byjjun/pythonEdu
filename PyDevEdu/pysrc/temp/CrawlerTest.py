ef3fa92233910f8bfad3666e5f27d9a56f2a49aa
073860 <073860@1B4VWBX.daishin.sec>
2017-12-08 09:52:15 +0900

    #DaishinAutoLearning 1.0

--- PyDevEdu/pysrc/temp/CrawlerTest.py
+++ PyDevEdu/pysrc/temp/CrawlerTest.py
@@ -5,8 +5,10 @@ Created on 2017. 12. 7.
 '''
 
 import requests as rq
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from _ast import Pass
 
 
 #req = rq.get("http://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/etcann/DISDLSSubscribing.xml&divisionId=MDIS04007001000000&serviceId=SDIS04007001000#!")
@@ -20,13 +22,25 @@ driver.implicitly_wait(2)
 driver.find_element_by_name("p_userid").send_keys("ds073860")
 driver.find_element_by_name("p_pwd").send_keys("073860")
 driver.find_element_by_xpath('//*[@class="submit"]').click()
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@class="btn01"]').click()
 
-driver.implicitly_wait(2)
+if(len(driver.window_handles) > 1):
+    driver.switch_to_window(driver.window_handles[1])
 
-driver.get("http://daishin.ubob.com/Home/Index")
-driver.implicitly_wait(2)
+while True:
+    #c_url = driver.current_url
+    #print(c_url)
+    time.sleep(60)
+    try :
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/button[1]').click()
+    except:
+        Pass
+    print(driver.current_url)
 
-
+#driver.implicitly_wait(30)
+#c_url = driver.current_url
+#print(c_url)
 #soup = BeautifulSoup(html, 'html.parser')
 
 #print(soup)
--- PyDevEdu/pysrc/temp/DaishinAutoLearning.py
+++ PyDevEdu/pysrc/temp/DaishinAutoLearning.py
@@ -5,18 +5,42 @@ Created on 2017. 12. 7.
 '''
 
 import requests as rq
+import time
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from _ast import Pass
+
+
+#req = rq.get("http://dis.kofia.or.kr/websquare/index.jsp?w2xPath=/wq/etcann/DISDLSSubscribing.xml&divisionId=MDIS04007001000000&serviceId=SDIS04007001000#!")
+#html = req.text
 
 driver = webdriver.Chrome('C:\GitArea\hub_pythonEdu\lib\chromedriver.exe')
 
-driver.get("http://daishin.ubob.com/Account/Login")
+driver.get("http://learning.daishin.co.kr")
 driver.implicitly_wait(2)
 
-#driver.find_element_by_id('Username').send_keys('ds073860')
-driver.find_element_by_xpath('//input*[@id="Username"]').send_keys('ds073860')
+driver.find_element_by_name("p_userid").send_keys("ds073860")
+driver.find_element_by_name("p_pwd").send_keys("073860")
+driver.find_element_by_xpath('//*[@class="submit"]').click()
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@class="btn01"]').click()
 
-#driver.find_element_by_name("Username").send_keys("ds073860")
-driver.find_element_by_name("Password").send_keys("073860")
-driver.find_element_by_xpath('//*[@class="login_btn"]').click()
+if(len(driver.window_handles) > 1):
+    driver.switch_to_window(driver.window_handles[1])
 
-driver.implicitly_wait(2)
+while True:
+    #c_url = driver.current_url
+    #print(c_url)
+    time.sleep(60)
+    try :
+        driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/button[1]').click()
+    except:
+        Pass
+    print(driver.current_url)
+
+#driver.implicitly_wait(30)
+#c_url = driver.current_url
+#print(c_url)
+#soup = BeautifulSoup(html, 'html.parser')
+
+#print(soup)
