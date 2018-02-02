a80fb8033dc0649b2409b538eb0379cad95edae5
073860 <073860@1B4VWBX.daishin.sec>
2018-02-02 14:26:36 +0900

    xml to json

--- CrawlingByJJuN/src/crawling/SAVING/GetSavingJSON.py
+++ CrawlingByJJuN/src/crawling/SAVING/GetSavingJSON.py
@@ -13,9 +13,9 @@ apikey = 'c00ce82f75408d5a4d3bbe6c8db89dd2'
 
 def get_current_saving_json():
     request_url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth='+apikey+'&topFinGrpNo=020000&pageNo=1'
-    print request_url
+    #print request_url
     data = requests.post(request_url).content
-    print data
-    #return data
+    #print data
+    return data
     
 get_current_saving_json()
--- CrawlingByJJuN/src/crawling/SAVING/JSONParser4SAVING.py
+++ CrawlingByJJuN/src/crawling/SAVING/JSONParser4SAVING.py
@@ -6,19 +6,22 @@ Created on 2018. 1. 31.
 @author: 073860
 '''
 
-from SAVING import GetSavingXML
-import xml.etree.ElementTree as ET
+from SAVING import GetSavingJSON
+import json
+import pprint
 
 def savingparser():
     
-    savingxml = GetSavingXML.get_current_saving_xml()
+    savingjson = GetSavingJSON.get_current_saving_json()
     
-    print "####"
-    print savingxml
+    result_dic = json.loads(savingjson)
+    baselist_dic = result_dic['result']
     
-    tree = ET.fromstring(savingxml)
+    pprint.pprint(baselist_dic)
     
     
+    
+    '''
     for _result in tree.findall('result'):
         
         if _result.findall('err_code').text !='000':
@@ -34,41 +37,10 @@ def savingparser():
             
             
         print '은행이름 : ',saving_dic['FINNAME']
-            
+    '''
         
 
 savingparser()
     
-'''
-tree = ET.fromstring(elsxmldata)
-    
-    i=1
-            
-    els_dic_list = []
-    
-    for message in tree.findall('message'):
-        for elslist in message.findall('DISDlsListDTO'):
-            totalcount = elslist.find('dbio_total_count_')
-            
-            #if totalcount is not None:
-                #print "총 개수: ",totalcount.text   
-                
-            for oneels in elslist.findall('DISDlsDTO'):
-                #print("aaa")
-                #print(i)
-                #print(oneels)
-                
-                #Except ELS
-                nowdate = datetime.today().strftime("%Y%m%d")
-                if nowdate >= oneels.find('val26').text:
-                    #print "X ELS"
-                    continue
-                                
-                els_dic = {}
-                # ELS
-                elsname = oneels.find('val6')
-                if elsname is not None:    
-                    els_dic['ELSNAME']=elsname.text
-                #
-'''
+
     
