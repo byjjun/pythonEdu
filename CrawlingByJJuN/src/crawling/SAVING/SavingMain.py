63624546c94eacaffc702ed2a067a7f32627f697
073860 <073860@1B4VWBX.daishin.sec>
2018-04-27 11:45:38 +0900

    # V1.0 예금.적금.ELS 자동입력

--- CrawlingByJJuN/src/crawling/SAVING/SavingMain.py
+++ CrawlingByJJuN/src/crawling/SAVING/SavingMain.py
@@ -11,33 +11,74 @@ from SAVING import JSONParser4SAVING
 if __name__ == '__main__':
     pass
 
-#은행
-#정기예금 - 우대포함
-print datetime.today().strftime('%Y년 %m월 %d일 '),'기준  예금금리  Top 5 입니다.'
-print "<br/>"
-print "<hr/>"
-print "은행 우대금리 기준 Top 10"
-print "<br/>"
-topsaving_list=JSONParser4SAVING.savingparser('은행', '예금', 1)
-print MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+def str_saving():
+    
+    #은행
+    #정기예금 - 우대포함
+    str_return = datetime.today().strftime('%Y년 %m월 %d일 ')+'기준'  
+    str_return += '은행 / 저축은행 정기예금  Top 10.'
+    str_return +=  "<br/>"
+    str_return +=  "<hr/>"
+    str_return +=  "은행  12개월 정기예금  - 우대금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('은행', '예금', 1)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    
+    #정기예금 - 우대 비포함
+    str_return +=  "<hr/>"
+    str_return +=  "은행  12개월 정기예금 - 단순금리  Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('은행', '예금', 2)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    
+    #저축은행
+    #정기예금 - 우대포함
+    str_return +=  "<hr/>"
+    str_return +=  "저축은행 12개월 정기예금 - 우대금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('저축은행', '예금', 1)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    #정기예금 - 우대비포함
+    str_return +=  "<hr/>"
+    str_return +=  "저축은행 12개월 정기예금 - 일반금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('저축은행', '예금', 2)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    
+    return str_return
 
-#정기예금 - 우대 비포함
-print "<hr/>"
-print "은행 단순금리 기준 Top 10"
-print "<br/>"
-topsaving_list=JSONParser4SAVING.savingparser('은행', '예금', 2)
-print MakeSAVINGHtml.makeproduchtml(topsaving_list)
 
-#저축은행
-#정기예금 - 우대포함
-print "<hr/>"
-print "저축은행 우대금리기준 Top 10"
-print "<br/>"
-JSONParser4SAVING.savingparser('저축은행', '예금', 1)
-print MakeSAVINGHtml.makeproduchtml(topsaving_list)
-#정기예금 - 우대비포함
-print "<hr/>"
-print "저축은행 일반금리기준 Top 10"
-print "<br/>"
-topsaving_list=JSONParser4SAVING.savingparser('저축은행', '예금', 2)
-print MakeSAVINGHtml.makeproduchtml(topsaving_list)
+def str_inssaving():
+     #은행
+    #정기적금 - 우대포함
+    str_return =  datetime.today().strftime('%Y년 %m월 %d일 ')+'기준  적금금리  Top 10 입니다.'
+    str_return +=  "<br/>"
+    str_return +=  "<hr/>"
+    str_return +=  "은행  12개월 정기적금  - 우대금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('은행', '적금', 1)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    #정기적금 - 우대 비포함
+    str_return +=  "<hr/>"
+    str_return +=  "은행  12개월 정기적금 - 단순금리  Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('은행', '적금', 2)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    
+    #저축은행
+    #정기적금 - 우대포함
+    str_return +=  "<hr/>"
+    str_return +=  "저축은행 12개월 정기적금 - 우대금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('저축은행', '적금', 1)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    #정기적금 - 우대비포함
+    str_return +=  "<hr/>"
+    str_return +=  "저축은행 12개월 정기적금 - 일반금리 Top 10"
+    str_return +=  "<br/>"
+    topsaving_list=JSONParser4SAVING.savingparser('저축은행', '적금', 2)
+    str_return += MakeSAVINGHtml.make_str_produchtml(topsaving_list)
+    return str_return
+
+#print str_saving()
+#print str_inssaving()
--- CrawlingByJJuN/src/writing/AutoPostWrite.py
+++ CrawlingByJJuN/src/writing/AutoPostWrite.py
@@ -6,33 +6,42 @@ Created on 2018. 3. 17.
 '''
 
 from selenium import webdriver
-from time import time
+from time import time, sleep
 from datetime import datetime
 
 
 from ELS import XmlParser4ELS
 from ELS import MakeELSHtml
+from SAVING import SavingMain
 
 def saving_title():
     #datetime.today().strftime('%Y년 %m월 %d일 '),'기준 '
-    saving_title = datetime.today().strftime('%Y년 %m월 %d일 '),'은행/저축은행 예금, 적금 추천순위 Top5 '
+    saving_title = datetime.today().strftime('%Y년 %m월 %d일 '),'은행/저축은행 예금 추천순위 Top 10 '
     return saving_title
 def saving_tag():
-    saving_tag = '고금리, 예금, 예금추천, 정기예금, 적금, 특판, 우대금리'
+    saving_tag = '고금리, 예금, 예금추천, 정기예금, 적금, 특판, 우대금리',''.decode('utf-8').encode('utf-8')
     return saving_tag
 def saving_post():
-    saving_post = ''
-    
+    saving_post = SavingMain.str_saving()
     return saving_post
 
-
+def ins_saving_title():
+    #datetime.today().strftime('%Y년 %m월 %d일 '),'기준 '
+    ins_saving_title = datetime.today().strftime('%Y년 %m월 %d일 '),'은행/저축은행 적금 추천순위 Top 10 '
+    return ins_saving_title
+def ins_saving_tag():
+    ins_saving_tag = '고금리, 적금, 적금추천, 예금추천, 정기적금, 예금, 특판, 우대금리',''.decode('utf-8').encode('utf-8')
+    return ins_saving_tag
+def ins_saving_post():
+    ins_saving_post = SavingMain.str_inssaving()
+    return ins_saving_post
 
 def els_title():
     #datetime.today().strftime('%Y년 %m월 %d일 '),'기준 '
     els_title = datetime.today().strftime('%Y년 %m월 %d일 '),'-청약중인 ELS목록 모아보기 '
     return els_title
 def els_tag():
-    els_tag = 'ELS랭킹, ELS순위, ELS추천, 고금리, 예금, 적금, 투자, 특판, 청약','UTF-8'.decode('utf-8').encode('utf-8')
+    els_tag = 'ELS랭킹, ELS순위, ELS추천, 고금리, 예금, 적금, 투자, 특판, 청약',''.decode('utf-8').encode('utf-8')
     return els_tag
 def els_post():
     elsdiclist = XmlParser4ELS.elsparser()
@@ -49,8 +58,9 @@ def write_init():
     
     login_id = 'byjjun@gmail.com'
     login_pw = 'POCKTAN1'
+    
     print login_id
-    #driver.implicitly_wait(1000)
+    driver.implicitly_wait(1000)
     driver.find_element_by_id('user_login').click()
     driver.find_element_by_id('user_login').send_keys(login_id)
     #driver.implicitly_wait(1000)
@@ -61,11 +71,11 @@ def write_init():
     return driver
 
 
-def write_post(driver, category, tag, post_content):
+def write_post(driver,category,title, tag, post_content):
     
     ### step 2 , write els post title ###
     driver.get("http://fundingchoice.co.kr/wp-admin/post-new.php")
-    driver.find_element_by_xpath('//*[@id="title"]').send_keys(els_title())
+    driver.find_element_by_xpath('//*[@id="title"]').send_keys(title)
     
     ### step 3, write tag ###
     driver.find_element_by_xpath(category).click()
@@ -87,4 +97,11 @@ def write_post(driver, category, tag, post_content):
 
 #write_post(userid, userpw)
 driver = write_init()
-write_post(driver,'//*[@id="in-category-17"]', els_tag(),els_post())
+sleep(2)
+write_post(driver,'//*[@id="in-category-17"]', els_title(), els_tag(),els_post())
+driver.implicitly_wait(1000)
+sleep(2)
+write_post(driver,'//*[@id="in-category-17"]', saving_title(), saving_tag(), saving_post())
+driver.implicitly_wait(1000)
+sleep(2)
+write_post(driver,'//*[@id="in-category-17"]', ins_saving_title(), ins_saving_tag(),ins_saving_post())
