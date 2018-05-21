b781d86656ceaeff25be9e04c11fca542861702f
073860 <073860@1B4VWBX.daishin.sec>
2018-05-21 17:49:48 +0900

    .

--- CrawlingByJJuN/src/crawling/STOCK/GetHKConsenStockPrice.py
+++ CrawlingByJJuN/src/crawling/STOCK/GetHKConsenStockPrice.py
@@ -11,7 +11,21 @@ import requests
 from selenium import webdriver
 from datetime import datetime
 from bs4 import BeautifulSoup
-from telnetlib import theNULL
+
+
+def getCurrentStockPriceDAUM(stock_code):
+    
+    request_url = 'http://finance.daum.net/item/main.daum?code='+stock_code
+    #print request_url
+    
+    driver = webdriver.PhantomJS(phantomjs)
+    driver.get(request_url)
+    
+    stock_now_price = driver.find_element_by_xpath('//*[@id="topWrap"]/div[1]/ul[2]/li[1]/em')
+    #print stock_code
+    #print stock_now_price   
+    
+    return stock_now_price.text
 
 def getCurrentStockConsenFromHK():
     
@@ -23,9 +37,9 @@ def getCurrentStockConsenFromHK():
     #request_url = 'http://hkconsensus.hankyung.com/apps.analysis/analysis.list?skinType=stock_good&sdate=2018-05-08&edate=2018-05-08&up_down_type=1&pagenum=150&order_type=&now_page=1&order_type=10010000'
     request_url = 'http://hkconsensus.hankyung.com/apps.analysis/analysis.list?skinType=stock_good&sdate='+today_str+'&edate='+today_str+'&up_down_type=1&pagenum=150&order_type=&now_page=1&order_type=10010000'
     
-    driver = webdriver.PhantomJS('C:\\phantomjs2.1.1\\bin\\phantomjs.exe')
+    driver = webdriver.PhantomJS(phantomjs)
     driver.get(request_url)
-    html = driver.page_source
+    #html = driver.page_source
     table_element = driver.find_element_by_xpath('//*[@id="contents"]/div[2]/table/tbody')
     tablebody_html = table_element.get_attribute('innerHTML')
     #print tablebody_html
@@ -50,12 +64,12 @@ def getCurrentStockConsenFromHK():
                 stock_dic['all_title'] = astock_data.find('strong').text
                 all_title = stock_dic['all_title']
                 start = all_title.find('(')+1
-                end = all_title.find(')')
+                end = start+6    
                 stock_code = all_title[start:end]
                 stock_dic['stock_code']=stock_code
                 stock_name = all_title[0:all_title.find('(')] 
                 stock_dic['stock_name']=stock_name
-                title = all_title[all_title.find(')')+1:len(all_title)]
+                title = all_title[end+1:len(all_title)]
                 stock_dic['title']=title
                 #print stock_dic['all_title']
                 #print stock_dic['stock_code']
@@ -80,57 +94,14 @@ def getCurrentStockConsenFromHK():
         upper_rate=int((upper_rate-1)*100)        
         stock_dic['upper_rate']=upper_rate
         #print str(stock_dic['upper_rate'])+'%'
+        stock_dic['now_price']=getCurrentStockPriceDAUM(stock_dic['stock_code'])
         stock_dic_list.append(stock_dic)
-        
+                
         print stock_dic
         
-    '''
-    #parser = etree.XMLParser(recover=True)
-    parser = ET.XMLParser(encoding="utf-8")
-    tree = ET.fromstring(tablebody_html, parser=parser)
+    return stock_dic_list
     
-    stock_conses_dic_list = []
-    
-    for stockdata_lst in tree.findAll('tr'):
-        for stockdata in stockdata_lst:
-            print stockdata.findAll('td').text
-        
-        
-    '''
-    
-    #print tablebody_html
-    
-    
-    
-    #title = table_element.find_element_by_xpath('//*[@id="content_477657"]/strong')
-    #print title.text
-    
-    #print table_element
-    
-    #html = table_element.text
-    
-    #print html
-    return html
-
-    '''    
-    session = requests.Session()
-    request_url = 'http://hkconsensus.hankyung.com/apps.analysis/analysis.list?skinType=stock_good&sdate=2018-05-11&edate=2018-05-11&up_down_type=4&pagenum=150&order_type=&up_down_type=1#'
-    #request_url = 'http://hkconsensus.hankyung.com/apps.analysis/analysis.list?skinType=stock_good&sdate='+today_str+'&edate='+today_str+'&order_type=&pagenum=150&now_page=1'
-                       
-    print request_url
-    raw_data = session.get(request_url)
-    #raw_data = requests.get(request_url)
-    
-    print raw_data.status_code
-    html = raw_data.text
-    
-    #print '-----'
-    print html
-    #print '-----'
-    '''
-    
-    
-    
+phantomjs='C:\\phantomjs2.1.1\\bin\\phantomjs.exe'
 
 getCurrentStockConsenFromHK()
 
