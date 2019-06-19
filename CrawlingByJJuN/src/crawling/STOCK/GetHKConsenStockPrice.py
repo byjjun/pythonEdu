09f31531ddbf1b4262a2bdcb662236942f3c5e54
073860 <073860@1B4VWBX.daishin.sec>
2019-06-19 15:16:30 +0900

    #현재가는 네이버에서

--- CrawlingByJJuN/src/crawling/STOCK/GetHKConsenStockPrice.py
+++ CrawlingByJJuN/src/crawling/STOCK/GetHKConsenStockPrice.py
@@ -15,6 +15,65 @@ from telnetlib import theNULL
 
 phantomjs='C:\\phantomjs2.1.1\\bin\\phantomjs.exe'
 
+
+'''
+현재가를 네이버에서 들고오는걸로 변경(지금버전 2019.6.19)
+'''
+def getCurrentStockPriceNaver(stock_code):
+    
+    stock_price = {}
+    if stock_code == '130960':
+        stock_code = '035760'
+    
+    request_url = 'https://finance.naver.com/item/main.nhn?code='+stock_code
+        
+    driver = webdriver.PhantomJS(phantomjs, service_args=['--cookies-file=/tmp/cookies.txt'])
+    
+    try:
+        
+        driver.get(request_url)
+        #print request_url + "aaa"
+        
+        stock_price_info_element = driver.find_element_by_xpath('//*[@id="middle"]/dl/dd[4]')
+        stock_price_info = stock_price_info_element.get_attribute('innerHTML')
+        #print stock_price_info
+                
+        stock_price_info_array = stock_price_info.split(' ')
+        
+        #print stock_price_info_array[1]
+        #print stock_price_info_array[5]
+        #print stock_price_info_array[6]
+        
+        #stock_updown_rate = driver.find_element_by_xpath('//*[@id="disArr[0]"]/span')
+        #print stock_updown_rate.text
+        plusminus = stock_price_info_array[5]
+        
+        if(plusminus == u'플러스'):
+            plusminus = u'+'
+            print '+'
+        elif(plusminus == u'마이너스'):
+            plusminus = u'-'
+            print '-'
+                
+        stock_price['now_price'] = stock_price_info_array[1].encode('utf-8')
+        stock_price['updown_rate'] = plusminus.encode('utf-8')+stock_price_info_array[6].encode('utf-8')
+    
+    except Exception as e:
+        print(e)
+        stock_price['now_price'] = "0"
+        stock_price['updown_rate'] = "0"
+    
+    #print stock_code
+    #print stock_now_price.text
+    #print stock_updown_rate.text
+    
+    #print stock_now_price.text+' ('+stock_updown_rate+')'
+    #driver.service.process.send_signal(signal.SIGTERM)
+    driver.quit()
+        
+    return stock_price
+
+
 '''
 DAUM에서 주식 현재가 추출
 '''
@@ -284,7 +343,7 @@ def getCurrentStockConsenFromHK():
         stock_dic['upper_rate']=str(upper_rate)
         #print str(stock_dic['upper_rate'])+'%'
         
-        now_stock_price = getCurrentStockPriceMK(stock_dic['stock_code'])
+        now_stock_price = getCurrentStockPriceNaver(stock_dic['stock_code'])
                 
         stock_dic['now_price']=now_stock_price['now_price']
         stock_dic['now_updown_rate']=now_stock_price['updown_rate']
