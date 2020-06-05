12b6fc2c2b5d577a14e4cf8f5e07d43ad7fadff0
073860 <073860@1B4VWBX.daishin.sec>
2020-06-05 16:08:04 +0900

    # Stock Upturn V1.0

--- ChoiceStock/src/main/StarterUpturn.py
+++ ChoiceStock/src/main/StarterUpturn.py
@@ -23,18 +23,9 @@ def main(login_pw):
     Preference.setWebDriverInit()
     
     try:
-        #최근 몇개 주식을 가지고 올것이냐(250)
-        Preference.setStockLoadCount("250")
-        #250
-        #괴리율 랭킹 몇등까지 표출
-        Preference.setStockRankCount(30)
-        #20
+        
          #이전목표주가는 몇개까지 표출
         Preference.setPrePriceCount(7)
-        #7
-        #몇일전 보고서 까지 찾을꺼냐
-        Preference.setStockDaysCount(14)
-        #14
             
         stock_dic_list = GetStockListfromHK.getUpturnStockFromHK()
         
