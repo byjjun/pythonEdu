d7e3ed40e7f336645603cb5dbd1149be324ac71f
073860 <073860@1B4VWBX.daishin.sec>
2020-06-16 10:46:38 +0900

    #sendemail bug 수정

--- ChoiceStock/src/write/SendEmail.py
+++ ChoiceStock/src/write/SendEmail.py
@@ -20,20 +20,22 @@ def sendMailtoGmail(title,contents, mail_pw):
     sender_email = "stockchoicebyjjun@gmail.com"
     receiver_email = "stockchoicebyjjun@gmail.com"
     
-    message=MIMEMultipart("alternative")
-    message["Subject"]=''.join(title)
-    message["From"]=sender_email
-    message["To"]=receiver_email
-    
-    
-    html=''.join(contents)
-    
-    part2 = MIMEText(html, "html")
-    message.attach(part2)
-    
-    smtp_session = smtplib.SMTP_SSL(smtp_server,port)
-    smtp_session.login(sender_email, mail_pw)
-    
-    smtp_session.sendmail(sender_email,receiver_email, message.as_string())
-    smtp_session.quit()
+    try:
+        message=MIMEMultipart("alternative")
+        message["Subject"]=''.join(title)
+        message["From"]=sender_email
+        message["To"]=receiver_email
+        
+        html=''.join(contents)
+        
+        part2 = MIMEText(html, "html")
+        message.attach(part2)
+        
+        smtp_session = smtplib.SMTP_SSL(smtp_server,port)
+        smtp_session.login(sender_email, mail_pw)
+        
+        smtp_session.sendmail(sender_email,receiver_email, message.as_string())
+        
+    finally:
+        smtp_session.quit()
 
