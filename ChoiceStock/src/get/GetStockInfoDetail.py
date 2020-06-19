ba618804db465138c4126a7f48f213ff935a87b5
073860 <073860@1B4VWBX.daishin.sec>
2020-06-19 10:30:26 +0900

    # 기준거래량을 5일평균으로 변경

--- ChoiceStock/src/get/GetStockInfoDetail.py
+++ ChoiceStock/src/get/GetStockInfoDetail.py
@@ -20,7 +20,8 @@ def getStockDeatilInfofromPaxnet(stock_code):
     
     try:
                 
-        driver.get(request_url)    
+        driver.get(request_url)
+        
         #시가총액
         stock_info_element = driver.find_element_by_xpath('//*[@id="contents"]/div[1]/div[2]/div[1]/div/table/tbody/tr[8]/td[1]')
         stock_info['total_cap']= stock_info_element.get_attribute('innerHTML')
@@ -31,13 +32,32 @@ def getStockDeatilInfofromPaxnet(stock_code):
         stock_info['today_volume']= stock_info_element.get_attribute('innerHTML')
         print stock_info['today_volume']
         
-        #전일 거래량
-        stock_info_element = driver.find_element_by_xpath('//*[@id="contents"]/div[1]/div[2]/div[1]/div/table/tbody/tr[8]/td[2]')
-        stock_info['yesterday_volume']= stock_info_element.get_attribute('innerHTML')
-        print stock_info['yesterday_volume'] 
+        #전일 거래량 -> 5일평균으로 변경
+        #stock_info_element = driver.find_element_by_xpath('//*[@id="contents"]/div[1]/div[2]/div[1]/div/table/tbody/tr[8]/td[2]')
+        #stock_info['everage_5day_volume']= stock_info_element.get_attribute('innerHTML')
+        #print stock_info['everage_5day_volume'] 
         
-        #거래량전일대비
-        volume_ratio =  float(stock_info['today_volume'].replace(',',''))/float(stock_info['yesterday_volume'].replace(',',''))
+        # 5일평균 거래량
+        volume_bf_days_element = driver.find_element_by_xpath('//*[@id="viewHtml2"]/tr[1]/td[8]')
+        volume_bf_1day = float(volume_bf_days_element.get_attribute('innerHTML').replace(',',''))
+        
+        volume_bf_days_element = driver.find_element_by_xpath('//*[@id="viewHtml2"]/tr[2]/td[8]')
+        volume_bf_2day = float(volume_bf_days_element.get_attribute('innerHTML').replace(',',''))
+        
+        volume_bf_days_element = driver.find_element_by_xpath('//*[@id="viewHtml2"]/tr[3]/td[8]')
+        volume_bf_3day = float(volume_bf_days_element.get_attribute('innerHTML').replace(',',''))
+        
+        volume_bf_days_element = driver.find_element_by_xpath('//*[@id="viewHtml2"]/tr[4]/td[8]')
+        volume_bf_4day = float(volume_bf_days_element.get_attribute('innerHTML').replace(',',''))
+        
+        volume_bf_days_element = driver.find_element_by_xpath('//*[@id="viewHtml2"]/tr[5]/td[8]')
+        volume_bf_5day = float(volume_bf_days_element.get_attribute('innerHTML').replace(',',''))
+        
+        everage_5day_volume = float((volume_bf_1day+volume_bf_2day+volume_bf_3day+volume_bf_4day+volume_bf_5day)/5)
+        
+        
+        #거래량 5일평균 대비
+        volume_ratio =  float(stock_info['today_volume'].replace(',',''))/everage_5day_volume
         volume_ratio = int(volume_ratio*100)
         stock_info['volume_ratio']=str(volume_ratio)
         print stock_info['volume_ratio']
